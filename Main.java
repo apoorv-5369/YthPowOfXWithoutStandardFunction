8997a1e48bc2118233a20bef8f44d4a5d9e1b847
Apoorv Varshney <85228469+apoorv-5369@users.noreply.github.com>
2022-01-19 23:37:30 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,12 +3,14 @@ public class MyClass {
     public static void main(String args[]) {
         int x, y, nthPowOfX;
       Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the value of x: "); 
       x=sc.nextInt();
+      System.out.println("Enter the value of y: ");
       y=sc.nextInt();
       nthPowOfX=x;
       for(int i=1;i<y;i++){
           nthPowOfX*=x;
       }
-      System.out.println(nthPowOfX);
+      System.out.println("Yth Power of X is: " + nthPowOfX);
     }
 }
